57da717263f41c9f482370e0159b5d5035dc7ce6
jefftdb <jefftdb@gmail.com>
2025-04-13 20:42:52 -0300

    Acertando os erros do merg

--- App.js
+++ App.js
@@ -59,28 +59,45 @@ function LogoutScreen({ onLogout }) {
 // Tela de Login
 function LoginScreen({ navigation, onLogin, biometria, autenticar }) {
   return (
-    <View style={styles.container}>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Image source={require("./assets/icon.png")} style={styles.img} />
-        <TextInput placeholder="Usuário" style={styles.input} />
-        <TextInput placeholder="Senha" secureTextEntry style={styles.input} />
-        <View style={styles.iconContainer}>
-          <Button title="Entrar" onPress={onLogin} />
-          <Button title="Cadastrar" color={"red"} onPress={() => navigation.navigate("Cadastrar")} />
-        </View>
-      </View>
-
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>
-          {biometria
-            ? "Ou faça o login com biometria"
-            : "Dispositivo não compatível com biometria"}
-        </Text>
-        <TouchableOpacity onPress={autenticar}>
-          <Image source={require("./assets/digital.png")} style={styles.img} />
-        </TouchableOpacity>
-        <StatusBar style="auto" />
-      </View>
+    <View style = {styles.container}>
+                <Animatable.View animation = 'fadeInLeft' delay={500} style = {styles.containerHeader}>
+                <Text style = {styles.message}>Bem-vindo(a)</Text>
+                </Animatable.View>
+    
+                <Animatable.View animation = 'fadeInUp' style = {styles.containerForm}>
+                    <Text styles = {styles.title}>Email</Text>
+    
+                    <TextInput
+                        placeholder = "Digite um email..."
+                        style = { styles.input}
+                    />
+    
+                    <Text styles = {styles.title}>Senha</Text>
+                    <TextInput
+                        placeholder = "Digite a sua senha"
+                        style = { styles.input}
+                    />
+    
+                    <TouchableOpacity style = {styles.button}
+                        onPress={onLogin}>
+                        <Text style = {styles.buttonText}>Acessar</Text>
+                    </TouchableOpacity>
+    
+                    <TouchableOpacity style = {styles.buttonRegister} onPress={() => navigation.navigate('Cadastrar')}>
+                        <Text style = {styles.registerText}>Não possui uma conta? Cadastre-se</Text>
+                    </TouchableOpacity>
+                    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                      <Text>
+                        {biometria
+                          ? "Ou faça o login com biometria"
+                          : "Dispositivo não compatível com biometria"}
+                      </Text>
+                      <TouchableOpacity onPress={autenticar}>
+                        <Image source={require("./assets/digital.png")} style={styles.img} />
+                      </TouchableOpacity>
+                      <StatusBar style="auto" />
+                    </View>
+                </Animatable.View>
     </View>
   );
 }
@@ -136,87 +153,63 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  img:{
-    width:100,
-    height:100,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#4169E1',
-},
-containerHeader: {
-   marginTop: '14%',
-   marginBottom: '8%',
-   paddingStart: '5%',   
-},
-message: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#FFF'
-},
-containerForm: {
-    backgroundColor: '#FFF',
-    flex: 1,
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-    paddingStart: '5%',
-    paddingEnd: '5%'
-},
-title:{
-    fontSize: 20,
-    marginTop: 28,        
-},
-input: {
-    borderBottomWidth: 1,
-<<<<<<< HEAD
-    height: 40,
-    marginBottom: 12,
-    fontSize: 16,
-},
-button: {
-    backgroundColor: '#4169E1',
-    widht: '100%',
-    borderRadius: 4,
-    paddingVertical: 8,
-    marginTop: 14,
-    justifyContent: 'center',
-    alignItems: 'center'
-},
-buttonText: {
-    color: '#FFF',
-    fontSize: 18,
-    fontWeight: 'bold',
-},
-buttonRegister: {
-    marginTop: 14,
-    alignSelf: 'center',
-},
-registerText: {
-    color: '#a1a1a1'
-}
-=======
-    width: 200,
-    marginBottom: 20,
-    padding: 10,
-  },
-  img: {
-    width: 100,
-    height: 100,
-  },
-  button: {
-    padding: 10,
-    backgroundColor: "blue",
-    borderRadius: 5,
-    marginTop: 10,
-    
-  },
-  iconContainer: {
-    flexDirection: "row", 
-    justifyContent: "space-around", 
-    alignItems: "center", 
-    marginVertical: 10,
-    marginHorizontal:10,
-     
-  },
-});
+const styles = StyleSheet.create ({
+    container: {
+        flex: 1,
+        backgroundColor: '#4169E1',
+    },
+    containerHeader: {
+       marginTop: '14%',
+       marginBottom: '8%',
+       paddingStart: '5%',   
+    },
+    message: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#FFF'
+    },
+    containerForm: {
+        backgroundColor: '#FFF',
+        flex: 1,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%'
+    },
+    title:{
+        fontSize: 20,
+        marginTop: 28,        
+    },
+    input: {
+        borderBottomWidth: 1,
+        height: 40,
+        marginBottom: 12,
+        fontSize: 16,
+    },
+    button: {
+        backgroundColor: '#4169E1',
+        widht: '100%',
+        borderRadius: 4,
+        paddingVertical: 8,
+        marginTop: 14,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    buttonRegister: {
+        marginTop: 14,
+        alignSelf: 'center',
+    },
+    registerText: {
+        color: '#a1a1a1'
+    },
+    img:{
+      width:100,
+      height:100,
+    }
+
+})
--- components/inicio.js
+++ components/inicio.js
@@ -1,12 +1,81 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import * as Animatable from 'react-native-animatable';
+import {useNavigation} from '@react-navigation/native';
+import {RegistraProtocolo} from './registraProtocolo';
 
-export function Inicio() {
-  
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style = {styles.text}>Bem vindo ao app de processos.</Text>
-      <Text>Vá ao menu Registrar para criar sua solicitação!</Text>
-    </View>
-  );
+export function Inicio(){
+    const navigation = useNavigation();
+
+    return(
+        <View style = {styles.container}>
+            <View style = {styles.containerLogo}>
+                <Animatable.Image
+                   animation = "flipInY" 
+                   source = {require('../assets/logo2.jpg')} 
+                   style = {{width: '100%'}}
+                   resizeMode="contain"
+                />
+            </View>
+            <Animatable.View delay={600} animation = "fadeInUp" style = {styles.containerForm}>
+                <Text style = {styles.title}>Protocole sua solicitação em qualquer lugar
+                      de modo prático!
+                </Text>
+                <Text style = {styles.text}>REgistra agora mesmo.</Text>
+
+                <TouchableOpacity style = {styles.button}
+                onPress={ () => navigation.navigate('Registrar')}>
+                    <Text style = {styles.buttonText}>Registrar</Text>
+                </TouchableOpacity>
+
+            </Animatable.View>
+            
+        </View>
+    );
 }
+
+const styles = StyleSheet.create ({
+    container:{
+        flex: 1,
+        backgroundColor: "#4169E1"
+    },
+    containerLogo:{
+        flex: 2,
+         backgroundColor: "#4169E1",
+         justifyContent: 'center', 
+         alignItems: 'center'
+    },
+    containerForm: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        paddingStart: '5%',
+        paddingEnd: '5%',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 28,
+        marginBottom: 12,
+    },
+    text: {
+        color: '#a1a1a1'
+    },
+    button: {
+        position: 'absolute',
+        backgroundColor: '#4169E1',
+        borderRadius: 50,
+        paddingVertical: 8,
+        width: '60%',
+        alignSelf: 'center',
+        bottom: '15%', 
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        fontSize: 18,
+        color: '#FFF',
+        fontWeight: 'bold',
+    }
+})
